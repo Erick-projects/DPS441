925969f9ef6cc069c50c0f448bab8aa3c6a613db
Heysel2 <124595071+Heysel2@users.noreply.github.com>
2024-05-02 10:09:02 -0600

    Delete menu/components/Logout.js

--- menu/components/Logout.js
+++
@@ -1,9 +0,0 @@
-import React, {useState} from 'react';
-import { View, Text} from 'react-native';
-export default function Logout({ navigation }) {
-    return (
-    <View>
-        <Text>Aqui el login</Text>
-        </View>
-);
-}
