795bb2e1ab7d3405458705cb4801cc422de825be
CastroJC7 <124717271+CastroJC7@users.noreply.github.com>
2024-05-05 15:50:54 -0600

    Update Hogar.js

--- menu/components/Hogar.js
+++ menu/components/Hogar.js
@@ -1,9 +1,39 @@
-import React, {useState} from 'react';
-import { View, Text} from 'react-native';
-export default function Hogar({ navigation }) {
-    return (
+import React, { useState, useEffect } from 'react';
+import { View } from 'react-native';
+import ProductList from './ProductList';
+import { getProductos } from '../servicios/Produc.Service';
+
+const Hogar = ({ agregarAlCarrito }) => {
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const products = await getProductos();
+        setProductos(products);
+      } catch (error) {
+        console.error('Error al obtener los productos:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  const agregarAlCarritoLocal = (producto) => {
+    // Llama a la función agregarAlCarrito 
+    agregarAlCarrito(producto, 1);
+  };
+
+  return (
     <View>
-        <Text>categoria hogar aqui</Text>
-        </View>
-);
-}
+   
+      <ProductList
+        productos={productos} 
+        categoria="Hogar" 
+        agregarAlCarrito={agregarAlCarritoLocal} 
+      />
+    </View>
+  );
+};
+
+export default Hogar;
