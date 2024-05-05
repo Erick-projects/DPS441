ec3b8726eac91ceba54bada7e093d2ed8f805364
CastroJC7 <124717271+CastroJC7@users.noreply.github.com>
2024-05-05 15:48:04 -0600

    Update Deportes.js

--- menu/components/Deportes.js
+++ menu/components/Deportes.js
@@ -1,9 +1,40 @@
-import React, {useState} from 'react';
-import { View, Text} from 'react-native';
-export default function Deportes({ navigation }) {
-    return (
+import React, { useState, useEffect } from 'react';
+
+import { View } from 'react-native';
+import ProductList from './ProductList';
+import { getProductos } from '../servicios/Produc.Service';
+
+const Deportes  = ({ agregarAlCarrito }) => {
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const products = await getProductos();
+        setProductos(products);
+      } catch (error) {
+        console.error('Error al obtener los productos:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+ // Función para agregar un producto al carrito localmente
+  const agregarAlCarritoLocal = (producto) => {
+
+    // Llama a la función agregarAlCarrito con el producto y la cantidad
+    agregarAlCarrito(producto, 1);
+  };
+
+  return (
     <View>
-        <Text>categoria deportes aqui</Text>
-        </View>
-);
-}
+      <ProductList
+        productos={productos} 
+        categoria="Deportes" 
+        agregarAlCarrito={agregarAlCarritoLocal} 
+      />
+    </View>
+  );
+};
+
+export default Deportes;
