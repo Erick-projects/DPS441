57026cdd171984a98f483bd1728585b8d7ef73af
CastroJC7 <124717271+CastroJC7@users.noreply.github.com>
2024-05-05 15:50:04 -0600

    Update Electronicos.js

--- menu/components/Electronicos.js
+++ menu/components/Electronicos.js
@@ -1,9 +1,43 @@
-import React, {useState} from 'react';
-import { View, Text} from 'react-native';
-export default function Electronicos({ navigation }) {
-    return (
+import React, { useState, useEffect } from 'react';
+import { View } from 'react-native';
+import ProductList from './ProductList';
+import { getProductos } from '../servicios/Produc.Service'; 
+
+const Electronicos = ({ agregarAlCarrito }) => {
+  const [productos, setProductos] = useState([]); 
+
+
+  useEffect(() => {
+    
+    const fetchProducts = async () => {
+      try {
+        
+        const products = await getProductos();
+        
+        setProductos(products);
+      } catch (error) {
+        console.error('Error al obtener los productos:', error); 
+      }
+    };
+    fetchProducts(); // Llama a la función para obtener productos al montar el componente
+  }, []);
+
+  // Función para agregar un producto al carrito 
+  const agregarAlCarritoLocal = (producto) => {
+    // Llama a la función agregarAlCarrito
+    agregarAlCarrito(producto, 1);
+  };
+
+  return (
     <View>
-        <Text>categoria electronicos aqui</Text>
-        </View>
-);
-}
+   
+      <ProductList
+        productos={productos} 
+        categoria="Electrónicos" 
+        agregarAlCarrito={agregarAlCarritoLocal} 
+      />
+    </View>
+  );
+};
+
+export default Electronicos;
