dfd798cd671280256e3704fd10bceeb70f1e036f
CastroJC7 <124717271+CastroJC7@users.noreply.github.com>
2024-05-05 15:53:20 -0600

    Update Ropa.js

--- menu/components/Ropa.js
+++ menu/components/Ropa.js
@@ -1,9 +1,39 @@
-import React, {useState} from 'react';
-import { View, Text} from 'react-native';
-export default function Ropa({ navigation }) {
-    return (
+import React, { useState, useEffect } from 'react';
+import { View } from 'react-native';
+import ProductList from './ProductList';
+import { getProductos } from '../servicios/Produc.Service';
+
+const Ropa = ({ agregarAlCarrito }) => {
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const products = await getProductos();
+        setProductos(products);
+      } catch (error) {
+        console.error('Error al obtener los productos:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+ // Función para agregar un producto al carrito 
+  const agregarAlCarritoLocal = (producto) => {
+    // Llama a la función agregarAlCarrito
+    agregarAlCarrito(producto, 1);
+  };
+
+  return (
     <View>
-        <Text>categoria ropa aqui</Text>
-        </View>
-);
-}
+     
+      <ProductList
+        productos={productos} 
+        categoria="Ropa" 
+        agregarAlCarrito={agregarAlCarritoLocal} 
+      />
+    </View>
+  );
+};
+
+export default Ropa;
